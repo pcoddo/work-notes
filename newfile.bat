897578e0a57cb0fd94a20773c1e3adc355a34dd3
pcoddo <pcoddo@gmail.com>
2020-03-30 15:44:55 -0400

    Clear notes

--- newfile.bat
+++ newfile.bat
@@ -1,7 +1,19 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 
-
 echo %mydate%.md
-echo # Work :notebook_with_decorative_cover: for %mydate% >> %mydate%.md
+echo # Work notes for %mydate% >> %mydate%.md
+echo ### To do :green_book: >> %mydate%.md
+echo * >> %mydate%.md
+echo * >> %mydate%.md
+echo * >> %mydate%.md
+echo ### In Progress :ledger: >> %mydate%.md
+echo * >> %mydate%.md
+echo * >> %mydate%.md
+echo * >> %mydate%.md
+echo ### Completed :closed_book: >> %mydate%.md
+echo - [ ] . >> %mydate%.md
+echo - [ ] . >> %mydate%.md
+echo - [ ] . >> %mydate%.md
+
 
