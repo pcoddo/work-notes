4c8dc9466916201ea9620e792f09123e4f9e2c5b
pcoddo <pcoddo@gmail.com>
2020-03-30 15:33:11 -0400

    Test header

--- newfile.bat
+++ newfile.bat
@@ -1,3 +1,7 @@
 @echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 
-echo "Some **Markdown** text." | python -m markdown > %1.md
+
+echo %mydate%.md
+echo # Work :notebook_with_decorative_cover: for %mydate% >> %mydate%.md
+
