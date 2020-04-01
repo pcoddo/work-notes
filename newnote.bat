7cd1cb2fe0c63c7505fbe52489bddf0b59ac5f8a
pcoddo <pcoddo@gmail.com>
2020-04-01 09:44:37 -0400

    Daily update

--- newnote.bat
+++ newnote.bat
@@ -2,18 +2,16 @@
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 
 echo %mydate%.md
-echo # Work notes for %mydate% >> %mydate%.md
+echo ## Summary for *%mydate%* >> %mydate%.md
 echo ### To do :green_book: >> %mydate%.md
-echo * >> %mydate%.md
-echo * >> %mydate%.md
-echo * >> %mydate%.md
+echo - [] >> %mydate%.md
+echo - [] >> %mydate%.md
+echo - [] >> %mydate%.md
 echo ### In Progress :ledger: >> %mydate%.md
-echo * >> %mydate%.md
-echo * >> %mydate%.md
-echo * >> %mydate%.md
+echo - [] >> %mydate%.md
+echo - [] >> %mydate%.md
+echo - [] >> %mydate%.md
 echo ### Completed :closed_book: >> %mydate%.md
-echo - [ ] . >> %mydate%.md
-echo - [ ] . >> %mydate%.md
-echo - [ ] . >> %mydate%.md
+echo - [x] >> %mydate%.md
 
 
